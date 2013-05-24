8810c4a17c2244847d752ca455dffa1bc28080c8
daniela <dane_abril@hotmail.com>
2013-05-24 08:35:39 -0600

    inicio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Daniela::Application.config.secret_token = '39b3182c4a129fa2c5aff766c2776d46692f877f53c1bcfe7b08fc19214e91ed3c726ec83ccbf0ef06db63302069c9b037cf5fa1addb6387169fb5e23ef087f0'
